8126c2ad79772b8c82c347f7421abecb856eb586
Jason Abbott <dev@trailimage.com>
2020-02-19 17:45:49 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::Path;
+use toml;
+
+type Pairs = Vec<(String, String)>;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ExifConfig {
+    camera: Pairs,
+    software: Pairs,
+    lens: Pairs,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    categories: Vec<String>,
+    what: Vec<String>,
+    exif: ExifConfig,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("./public/config.toml");
+    let contents = fs::read_to_string(path).expect("Something went wrong reading the file");
+    //let parsed = contents.parse::<Value>().unwrap();
+
+    let config: Config = toml::from_str(&contents).unwrap();
+
+    println!("{}", config.what[0])
 }
