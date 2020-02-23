1b0ae15722b3ff8da5ec951c80be256a99b6494d
Jason Abbott <dev@trailimage.com>
2020-02-23 11:33:39 -0700

    wip

--- src/main.rs
+++ src/main.rs
@@ -14,64 +14,91 @@ pub use exif::EXIF;
 pub use photo::Photo;
 pub use post::Post;
 
-use std::io;
+use serde::Deserialize;
 use std::fs;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 use toml;
-use fs::DirEntry;
 
 fn main() {
     let root = Path::new("./public/");
+    let config: BlogConfig = load_config(root);
     let mut blog = Blog {
         posts: Vec::new(),
         categories: Vec::new(),
     };
-    let contents = fs::read_to_string(root.join("config.toml")).unwrap();
-    let config: BlogConfig = toml::from_str(&contents).unwrap();
-
-    //  for name in config.categories {
-    //      blog.categories.push(Category {
-    //          name,
-    //          posts: Vec::new(),
-    //      });
-    //  }
 
     for entry in fs::read_dir(root.join("img/")).unwrap() {
-        let entry: DirEntry = entry.unwrap();
-        let path: PathBuf = entry.path();
+        let path: PathBuf = entry.unwrap().path();
 
         if !path.is_dir() {
             continue;
         }
 
-        let series_posts: Vec<Post> = fs::read_dir(&path).unwrap()
-            .map(|e| e.unwrap().path())
-            .filter(|p| p.is_dir())
-            .map(load_post)
-            .collect();
+        for entry in fs::read_dir(path).unwrap() {
+            let path: PathBuf = entry.unwrap().path();
 
-        println!("{} series posts", series_posts.len());
-
-        if !series_posts.is_empty() {
-            for p in series_posts {
-               blog.posts.push(p);
+            match load_series(&path) {
+                Some(posts) => {
+                    println!("{} series posts", posts.len());
+                    for p in posts {
+                        blog.posts.push(p);
+                    }
+                    continue;
+                }
+                None => blog.posts.push(load_post(path.as_path())),
             }
-            continue;
         }
-
-        blog.posts.push(load_post(path));
     }
 
     println!("{} posts", blog.posts.len());
 }
 
+fn load_series<'a>(path: &Path) -> Option<Vec<Post<'a>>> {
+    let sub_dirs: Vec<PathBuf> = fs::read_dir(&path)
+        .unwrap()
+        .map(|e| e.unwrap().path())
+        .filter(|p| p.is_dir())
+        .collect();
 
+    if sub_dirs.is_empty() {
+        return None;
+    }
 
+    let config: SeriesConfig = load_config(path);
+    let series_posts: Vec<Post> = sub_dirs
+        .iter()
+        .map(|p| load_series_post(p.as_path(), &config))
+        .collect();
 
-fn load_post<'a>(path: PathBuf) -> Post<'a> {
-   Post {
-      path,
-      chronological: true,
-      ..Default::default()
-  }
+    Some(series_posts)
+}
+
+fn load_series_post<'a>(path: &Path, series: &SeriesConfig) -> Post<'a> {
+    let config: PostConfig = load_config(&path);
+    Post {
+        title: series.title,
+        sub_title: config.title,
+        summary: config.summary,
+        is_partial: true,
+        total_parts: series.parts,
+        chronological: true,
+        ..Post::default()
+    }
+}
+
+fn load_post<'a>(path: &Path) -> Post<'a> {
+    let config: PostConfig = load_config(&path);
+    Post {
+        title: config.title,
+        summary: config.summary,
+        is_partial: false,
+        total_parts: 0,
+        chronological: true,
+        ..Post::default()
+    }
+}
+
+fn load_config<'de, T: Deserialize<'de>>(path: &'de Path) -> T {
+    let contents = fs::read_to_string(path.join("config.toml")).unwrap();
+    toml::from_str(&contents).unwrap()
 }
